bbfda923e383aedd3d8b3cb8e6b478b6bc450066
Camila Vieira <camilafariavieira@gmail.com>
2020-01-22 17:10:41 -0300

    Basic operations script.js

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ console.log("No script.js");
 var firstName = 'John';
 var lastName = 'Smith';
 var age = 15;
+var fullAge = false;
+var job; //undefined
+job = 'Student';
 
-console.log(`${firstName + lastName}`  );
+console.log(`${firstName + lastName}`);
 console.log(firstName,lastName);
+
+/*Variable mutation and type coercion*/
+console.log(firstName + ' ' + age); //convert number into string
+
+var isMarried, sport;
+isMarried = false;
+sport = 'tennis';
+
+alert(firstName + ' plays ' + sport + 
+'. Is he married? ' + (isMarried ? 'Yes' : 'No'));
+
+lastName = prompt('What is his last name?');
+console.log(firstName,lastName);
+
+age = 'fifteen'; //variable mutation
+console.log(age);
+
+var ageMark = 22;
+
+//Logical operators
+console.log((ageMark < age));
+
+//typeof operator
+console.log(typeof ageMark);
+
